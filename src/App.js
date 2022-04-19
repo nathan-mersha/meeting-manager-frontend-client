55dc3edde9dd42d6f0b67cb3830b9a53f6a40127
nathanmersha@gmail.com <nathanmersha@gmail.com>
2022-04-19 11:37:08 +0300

    Testing Webhook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meeting Manager Client WebApp
+          Meeting Manager Client's WebApp
         </p>
         <a
           className="App-link"
